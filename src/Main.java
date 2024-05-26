b4c66ba0dd1c8a5b4c0b21c9eacaede582da5074
Vitaliy V <vitaliyvilkov@gmail.com>
2024-05-26 20:56:38 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2500;
+        int mile = 20;
+
+        int bonusMile = amount / mile;
+        System.out.println(bonusMile);
+
+    }
+}
